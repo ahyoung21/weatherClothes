213c1dd00b30e76f66b0270ea1d00aabd65e3f38
유아영 <ahyoung.db@gmail.com>
2022-12-19 17:59:39 +0900

    add redux persist

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,8 @@ import type { AppProps } from 'next/app';
 
 import { Provider } from 'react-redux';
 import { store } from '../store/store';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import Head from 'next/head';
 
@@ -13,25 +15,29 @@ import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from '../styles/global-style';
 import { theme } from '../styles/theme';
 
+const persistor = persistStore(store);
+
 function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta property="og:title" content="Weather forecast" />
-        <meta property="og:description" content="Weather forecast" />
-        <meta property="og:image" content="/preview.png" />
-        <link rel="icon" href="/favicon.ico" />
-        <title>Weather forecast</title>
-      </Head>
-      <GlobalStyle />
-      <ThemeProvider theme={theme}>
-        <Header />
-        <main>
-          <Component {...pageProps} />
-        </main>
-        <Footer />
-      </ThemeProvider>
+      <PersistGate loading={null} persistor={persistor}>
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta property="og:title" content="Weather forecast" />
+          <meta property="og:description" content="Weather forecast" />
+          <meta property="og:image" content="/preview.png" />
+          <link rel="icon" href="/favicon.ico" />
+          <title>Weather forecast</title>
+        </Head>
+        <GlobalStyle />
+        <ThemeProvider theme={theme}>
+          <Header />
+          <main>
+            <Component {...pageProps} />
+          </main>
+          <Footer />
+        </ThemeProvider>
+      </PersistGate>
     </Provider>
   );
 }
--- store/store.ts
+++ store/store.ts
@@ -1,16 +1,28 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { persistReducer } from 'redux-persist';
+import { combineReducers } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
 import userSlice from './modules/userSlice';
 import counterSlice from './modules/counterSlice';
 import { useDispatch as useDispatchBase, useSelector as useSelectorBase } from 'react-redux';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const reducers = combineReducers({
+  user: userSlice,
+  counter: counterSlice,
+});
+
+const persistedReducer = persistReducer(persistConfig, reducers);
 
 /**
  * Creates a store and includes all the slices as reducers.
  */
 export const store = configureStore({
-  reducer: {
-    user: userSlice,
-    counter: counterSlice,
-  },
+  reducer: persistedReducer,
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
